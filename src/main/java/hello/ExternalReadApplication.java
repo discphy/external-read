2d7f2c5153b29c8404c5cf39e1330485c2bfcfe8
discphy <dinophy@nate.com>
2023-12-26 08:28:51 +0900

    feat(add): Application properties added

--- src/main/java/hello/ExternalReadApplication.java
+++ src/main/java/hello/ExternalReadApplication.java
@@ -1,9 +1,14 @@
 package hello;
 
+import hello.config.MyDataSourceConfigV3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+//@Import(MyDataSourceEnvConfig.class)
+//@Import(MyDataSourceValueConfig.class)
+@Import(MyDataSourceConfigV3.class)
+@SpringBootApplication(scanBasePackages = {"hello.datasource", "hello.pay"})
 public class ExternalReadApplication {
 
     public static void main(String[] args) {
